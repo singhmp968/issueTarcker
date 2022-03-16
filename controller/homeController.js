5c3827767c904659f25c36a426619cd94d4c2adf
XinghMp968 <31666974+singhmp968@users.noreply.github.com>
2022-03-17 00:23:20 +0530

    added home logic

--- controller/homeController.js
+++ controller/homeController.js
@@ -1,5 +1,16 @@
+const ProjectList = require("../models/projectDetails");
+
 module.exports.home = function (req, res) {
   // Future Reference
   //   return res.send("hello");
-  return res.render("home");
+  ProjectList.find({}, function (err, projectDet) {
+    if (err) {
+      console.log("error", err);
+      return;
+    }
+    return res.render("home", {
+      project: projectDet,
+    });
+  });
+  //return res.render("home");
 };
