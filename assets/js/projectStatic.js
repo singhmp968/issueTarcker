286d29dba356b65feebddeb030b702c3106ce247
Rahul Singh yadav <rahulsinghmp968@gmail.com>
2022-03-22 23:10:21 +0530

    added som part

--- assets/js/projectStatic.js
+++ assets/js/projectStatic.js
@@ -1,6 +1,32 @@
-function selectlabel(e) {
+let valsel = $("#valueSelected");
+let selectedArr = [];
+function checkifAlreadySelectOrNot(strUser) {
+  check = false;
+  for (let i of selectedArr) {
+    if (i === strUser) {
+      check = true;
+      break;
+    }
+    check = false;
+  }
+  if (!check) {
+    selectedArr.push(strUser);
+  }
+}
+async function selectlabel(e) {
   // todo Need to complete
-  console.log(e.target.value);
+
+  let eVal = document.getElementById("selectBox");
+  let strUser = eVal.options[eVal.selectedIndex].text;
+  console.log(strUser);
+  checkifAlreadySelectOrNot(strUser);
+  console.log("dasda", selectedArr);
+  //valsel.append(`<div>${strUser}</div>`);
+  let taggedelement = "";
+  for (let i in selectedArr) {
+    taggedelement += `<div>${strUser}</div>`;
+  }
+  valsel.append(taggedelement);
 }
 let repo = $("#reportissueform");
 repo.submit(function (e) {
