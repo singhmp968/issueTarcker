b5bea5c9b39858d18e8428b8a70cc43829c5461b
Rahul Singh yadav <rahulsinghmp968@gmail.com>
2022-04-11 23:35:11 +0530

    connectd with atlas

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,8 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/issueTarcker");
+dbUrl =
+  "mongodb+srv://xinghmp968:dgOqFGMbP2SioGej@cluster0.za5iv.mongodb.net/issueTarcker?retryWrites=true&w=majority" ||
+  "mongodb://localhost/issueTarcker";
+mongoose.connect(dbUrl);
 const db = mongoose.connection;
 // error
 db.on("error", console.error.bind(console, "erroe connecting to db"));
