7d8b746ff56fc648e812629863f08fd6edfdb379
Rahul Singh yadav <rahulsinghmp968@gmail.com>
2022-04-12 23:14:42 +0530

    started with UI/UX design

--- index.js
+++ index.js
@@ -3,9 +3,22 @@ const app = express();
 const expressLayout = require("express-ejs-layouts");
 app.use(expressLayout);
 const db = require("./config/mongoose");
+const sassMiddlWare = require("node-sass-middleware");
 app.use(express.urlencoded());
 app.use("/", require("./routes"));
 port = 8000;
+const path = require("path");
+// node-sass-middleware configuration
+app.use(
+  sassMiddlWare({
+    src: "./assets/scss",
+    dest: "./assets/css",
+    debug: true,
+    outputStyle: "extended",
+    prefix: "/css",
+  })
+);
+
 app.use(express.static("./assets"));
 app.set("view engine", "ejs");
 app.set("views", "./views");
